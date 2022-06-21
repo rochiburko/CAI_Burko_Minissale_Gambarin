d419cdb0c38dfabdbccebd02235add4cb27956c7
Rosario Burko <rosario.burko@bbva.com>
2022-06-20 21:11:35 -0300

    Se crea clase envio y sucursal

---
+++ SistemaDeMensajeria/Envio.cs
@@ -0,0 +1,39 @@
+﻿namespace SistemaDeMensajeria
+{
+    public class Envio
+    {
+        public int numeroSeguimiento { get; set; }
+        public string tipoEnvio { get; set; } 
+        public double peso { get; set; } //Si es correspondencia no se solicitara el peso
+        public Sucursal sucursalOrigen { get; set; }
+        public string direccionDestino { get; set; }
+        private string archivoDatosSucursales = "../../datos/envios.txt";
+        
+        public Envio()
+        {
+        }
+
+        public int asignarNumeroDeSeguimiento()
+        {
+            int numeroSeguimiento = 0;
+            var stream = File.OpenRead(archivoDatosSucursales);
+            var reader = new StreamReader(stream);
+
+            while (!reader.EndOfStream)
+            {
+                var linea = reader.ReadLine();
+
+                string[] datos = linea.Split(';');
+
+                if (int.Parse(datos[0]) > numeroSeguimiento) {
+                    //Obtengo el ultimo numero de seguimiento asignado a un pedido
+                    numeroSeguimiento = (int.Parse(datos[0]));
+                }
+            }
+
+            stream.Close();
+
+            return numeroSeguimiento + 1;
+        }
+    }
+}
---
+++ SistemaDeMensajeria/Sucursal.cs
@@ -0,0 +1,62 @@
+﻿namespace SistemaDeMensajeria
+{
+    public class Sucursal
+    {
+        public string region;
+        public string localidad;
+        public string provincia;
+        public string codigoPostal;
+        private string archivoDatosSucursales = "../../datos/sucursales.txt";
+
+        public Sucursal()
+        {
+        }
+
+        public bool obtenerDatosDeSucursal(string localidad)
+        {
+            bool sucursalValida = false;
+            var stream = File.OpenRead(archivoDatosSucursales);
+            var reader = new StreamReader(stream);
+
+            while (!reader.EndOfStream)
+            {
+                var linea = reader.ReadLine();
+
+                string[] datos = linea.Split(';');
+
+                if (datos[0].Equals(localidad))
+                {
+                    region = datos[1];
+                    provincia = datos[2];
+                    codigoPostal = datos[3];
+                    sucursalValida = true;
+                    break;
+                }
+            }
+
+            stream.Close();
+            return sucursalValida;
+        }
+
+        public List<string> obtenerLocalidadesSucursales()
+        {
+            List<string> sucursales = new List<string>();
+
+            var stream = File.OpenRead(archivoDatosSucursales);
+            var reader = new StreamReader(stream);
+
+            while (!reader.EndOfStream)
+            {
+                var linea = reader.ReadLine();
+
+                string[] datos = linea.Split(';');
+
+                sucursales.Add(datos[0]);
+            }
+
+            stream.Close();
+
+            return sucursales;
+        }
+    }
+}
